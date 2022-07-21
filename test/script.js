c2db131e20842730d91738306287fe052e581011
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-07-21 14:22:36 -0500

    No more weeeeéeeeeeée

--- test/script.js
+++ test/script.js
@@ -2,4 +2,4 @@ import physics from "../../physics/script.js";
 let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 0, 50);
 testObj.velocity.x = 100;
 testObj.velocity.y = 100;
-testObj.start("gravity", 400, "down");
+testObj.start("gravity", 200, "down");
