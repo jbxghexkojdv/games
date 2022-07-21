e04e1b50c60ad00255d3f8b421f62c985abf479c
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-07-21 14:08:11 -0500

    Different path

--- test/script.js
+++ test/script.js
@@ -1,3 +1,5 @@
 import physics from "../../physics/script.js";
-let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 50, 100);
-testObj.start("gravity", 8, "down");
+let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 0, 50);
+testObj.speed.x = 100;
+testObj.speed.y = 100;
+testObj.start("gravity", 400, "down");
