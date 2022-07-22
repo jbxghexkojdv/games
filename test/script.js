b900b3b73583959a8378e7c85d5ea70c7bb058b9
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-07-22 07:17:52 -0500

    Comment for clarification

--- test/script.js
+++ test/script.js
@@ -1,4 +1,4 @@
-import physics from "../../physics/script.js";
+import physics from "../../physics/script.js"; //    element to use    hitbox location
 let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 0, 50);
 testObj.velocity.x = 100;
 testObj.velocity.y = 100;
