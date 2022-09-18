d3c619a0c3826c256d7c156ba3b50f05cbbf045d
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-09-18 13:52:21 -0500

    show hitboxes

--- test/script.js
+++ test/script.js
@@ -1,6 +1,6 @@
-import physics from "../../physics/script.js"; //    element to use    hitbox location
-let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 50, 100);
+import physics from "../../physics/script.js"; //    element to use     size  location motion hitbox
+let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 50, 100, true, true);
 testObj.start("gravity", 50, "down");
 testObj.start("collision");
-let testObj2 = new physics.Thing(document.getElementsByTagName("p")[0], 10, 10, 50, 50, false);
+let testObj2 = new physics.Thing(document.getElementsByTagName("p")[0], 10, 10, 50, 50, false, true);
 testObj2.start("collision");
