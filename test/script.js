a46d9782f43bd2c54db9c82564fbe535f04a1948
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-09-18 13:57:13 -0500

    error checking

--- test/script.js
+++ test/script.js
@@ -1,6 +1,10 @@
 import physics from "../../physics/script.js"; //    element to use     size  location motion hitbox
+try{
 let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 50, 100, true, true);
 testObj.start("gravity", 50, "down");
 testObj.start("collision");
 let testObj2 = new physics.Thing(document.getElementsByTagName("p")[0], 10, 10, 50, 50, false, true);
 testObj2.start("collision");
+}catch(e){
+  document.getElementsByTagName("p")[0].innerHTML = e.stack;
+}
