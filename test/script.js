ea7e48c4c19f67d13eb4d182d40bd148a6a6de84
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-09-17 23:18:56 -0500

    start collision

--- test/script.js
+++ test/script.js
@@ -1,3 +1,4 @@
 import physics from "../../physics/script.js"; //    element to use    hitbox location
 let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 50, 100);
 testObj.start("gravity", 8, "down");
+testObj.start("collision");
