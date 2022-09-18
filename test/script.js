658b42b43d0bdd519435c5fea7f18c2cf244f11f
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-09-18 09:58:47 -0500

    ok it does

--- test/script.js
+++ test/script.js
@@ -2,5 +2,5 @@ import physics from "../../physics/script.js"; //    element to use    hitbox lo
 let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 50, 100);
 testObj.start("gravity", 50, "down");
 testObj.start("collision");
-/* let testObj2 = new physics.Thing(document.getElementsByTagName("p")[0], 10, 10, 50, 50, false);
-testObj2.start("collision"); */
+let testObj2 = new physics.Thing(document.getElementsByTagName("p")[0], 10, 10, 50, 50, false);
+testObj2.start("collision");
