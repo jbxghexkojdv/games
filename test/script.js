99dca2d6db796b0b6c19b0135fecbfae3e41ddf9
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-09-17 23:17:47 -0500

    fall

--- test/script.js
+++ test/script.js
@@ -1,12 +1,3 @@
 import physics from "../../physics/script.js"; //    element to use    hitbox location
-try
-{
-let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 0, 50);
-testObj.velocity.x = 100;
-testObj.velocity.y = 100;
-testObj.start("gravity", 200, "down");
-}
-catch(e)
-{
-  document.getElementsByTagName("a")[0].innerHTML = e;
-}
+let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 50, 100);
+testObj.start("gravity", 8, "down");
