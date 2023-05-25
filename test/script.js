6a3f022e02ba6dfe9727df17e7bf533d9103ff43
jbxghexkojdv <jbxghexkojdv@gmail.com>
2023-05-25 14:23:51 -0500

    Totally different example that's bouncy

--- test/script.js
+++ test/script.js
@@ -1,10 +1,17 @@
-import physics from "../../physics/script.js"; //    element to use     size  location motion hitbox
-try{
-let testObj = new physics.Thing(document.getElementsByTagName("a")[0], 10, 10, 50, 100, true, true);
-testObj.start("gravity", 50, "down");
-testObj.start("collision");
-let testObj2 = new physics.Thing(document.getElementsByTagName("p")[0], 10, 10, 50, 50, false, true);
-testObj2.start("collision");
-}catch(e){
-  document.getElementsByTagName("p")[0].innerHTML = e.stack;
+const physics = (await import(location.origin == "https://jbxghexkojdv.github.io" ? "../../physics/script.js" : "../../mywebsite/physics/script.js")).default;
+// ^Imports physics
+let testObjects = [
+  new physics.Thing(document.getElementsByTagName("p")[0], 10, 10, 45, 90, true, true),
+  new physics.Thing(document.getElementsByTagName("p")[1], 10, 10, 45, 0, true, true),
+  new physics.Thing(document.getElementsByTagName("p")[2], 10, 10, 0, 45, true, true),
+  new physics.Thing(document.getElementsByTagName("p")[3], 10, 10, 90, 45, true, true),
+  new physics.Thing(document.getElementsByTagName("p")[4], 20, 20, 40, 40, false, true, 0.5),
+];
+for(let i of testObjects)
+{
+  i.start("collision");
 }
+testObjects[0].velocity.y = -25;
+testObjects[1].velocity.y = 25;
+testObjects[2].velocity.x = 25;
+testObjects[3].velocity.x = -25;
